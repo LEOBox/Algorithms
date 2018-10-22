8840ee55b364be13ae5bb5725891a53510da3d28
Xuzhe Dang <Xuzhe.Dang@email.asu.edu>
2018-10-22 23:03:51 +0800

    Update unique.cpp

--- Unique/unique.cpp
+++ Unique/unique.cpp
@@ -3,15 +3,14 @@
 #include <algorithm>
 using namespace std;
 
-//use left point as base point
+//use left indicator as base indicator
 void unique_left_point(vector<int> &v);
-//use right point as base point
+//use right indicator as base indicator
 void unique_right_point(vector<int> &v);
 
 int main() {
-	// your code goes here
 	vector<int> v = {3,2,6,3,7,4,6,7,8,1,0,3,5};
-	//unique_left_point(v);
+	unique_left_point(v);
 	unique_right_point(v);
 	
 	return 0;
@@ -33,16 +32,19 @@ void unique_left_point(vector<int> &v) {
 }
 
 void unique_right_point(vector<int> &v) {
+    //sort the vector
     sort(v.begin(), v.end());
+    //init left and right indicator	
     size_t L,R;
     
+    //unique the element and cout the number of each element in vector
     for (L=0,R=0; R < v.size(); ++R) {
         if (v[L] != v[R]) {
             std::cout << v[L] << ":" << R-L << std::endl;
             L = R;
         }
     }
-    
+    //don't forget the tail!!!
     if (v.size() > 0) {
         std::cout << v[L] << ":" << R-L << std::endl;
     }
